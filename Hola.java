97b2e06053e6c4c6df195c438fa66c4a30cb44aa
Rodrigo Ricardo Perez Memije <roderkmax@gmail.com>
2026-02-24 19:35:38 -0600

    Hola mundo

--- Hola.java
+++ Hola.java
@@ -1,7 +1,6 @@
 public class Hola {
 public static void main (String [] Args){
-System.out.println("Primer hola modificado");
-System.out.println("segundo hola mundo");
+System.out.println("hola mundo");
 
 
 }
